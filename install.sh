295c7f9b9e8f55cb35d2b83192dbc2979b2236f2
Emmanuel Vera <emm.vera@gmail.com>
2025-02-24 20:22:50 -0600

    Add cargo, nerdfont and layzgit installation scripts

--- install.sh
+++ install.sh
@@ -8,24 +8,36 @@ exit
 
 # refresh group && install basic software
 newgrp sudo
-sudo apt -y install git curl
+sudo apt -y install git curl lxpolkit expect stow
+
+# cargo / rust
+exec ./install-cargo-rust.exp
+source ~/.cargo/env
+cargo install tree-sitter-cli
+
+# Stow
+stow .
 
 # install latest neovim
-cd /tmp
+pushd /tmp
 git clone https://github.com/neovim/neovim.git
 pushd neovim
 git checkout stable
 sudo apt -y build-dep neovim
 cpack -G DEB && sudo apt -y install ./build/nvim-linux-*.deb
 popd
+popd
 
 #install wezterm
+pushd /tmp
 curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/wezterm-fury.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
 sudo apt update
 sudo apt -y install wezterm
+popd
 
 #install awesomewm
+pushd /tmp
 sudo apt -y build-dep awesome
 sudo apt -y build-dep awesome-extra
 sudo apt -y install libxcb-xfixes0-dev
@@ -34,3 +46,24 @@ pushd awesome
 make package
 sudo apt install ./build/awesome*.deb
 popd
+popd
+
+# TMUX
+pushd /tmp
+sudo apt -y build-dep tmux
+git clone https://github.com/tmux/tmux.git
+pushd tmux
+git checkout tmux-3.5a
+sh autogen.sh
+./configure
+make
+sudo make install
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+popd
+popd
+
+# Blueman
+sudo apt install blueman
+
+# Install nerd fonts
+exec ./nerdfonts-expect.exp 21
