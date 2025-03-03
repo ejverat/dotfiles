a71ef5b8e041bdc2338af36747090ba8fe5ced96
Emmanuel Vera <emm.vera@gmail.com>
2025-03-03 17:01:55 -0600

    Rofi and rocm installation

---
+++ install-rofi.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pushd /tmp
+git clone https://github.com/davatorium/rofi.git
+pushd rofi
+git checkout 1.7.8
+git submodule update --init
+sudo apt build-dep rofi-dev
+meson setup build
+ninja -C build
+sudo ninja -C build install
+popd
+popd
