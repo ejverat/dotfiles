53cf94c2913ef8ca3bff63722641fe33f2e7f454
Emmanuel Vera <emm.vera@gmail.com>
2025-06-27 00:23:15 -0600

    install nvim script

---
+++ install-nvim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# install latest neovim
+pushd /tmp
+git clone https://github.com/neovim/neovim.git
+pushd neovim
+git checkout stable
+sudo apt -y build-dep neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+pushd build
+cpack -G DEB && sudo apt -y install ./nvim-linux-*.deb
+popd
+popd
+popd
