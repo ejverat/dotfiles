3c88cc21c9fa1e751fea3ffbb332e744cd7a1854
Emmanuel Vera <emm.vera@gmail.com>
2025-07-22 00:58:35 -0600

    Updating neovim installer

--- installers/install-nvim.sh
+++ installers/install-nvim.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 # install latest neovim
+echo "NEOVIM"
+
+echo "Cloning and choosing latest stable..."
+
 pushd /tmp
 git clone https://github.com/neovim/neovim.git
 pushd neovim
 git checkout stable
+
+echo "Installing dependencies..."
 sudo apt -y build-dep neovim
+
+echo "Building..."
 make CMAKE_BUILD_TYPE=RelWithDebInfo
+
+echo "Generating .deb's and installing..."
 pushd build
 cpack -G DEB && sudo apt -y install ./nvim-linux-*.deb
+
 popd
 popd
 popd
 
 sudo echo "EDITOR=/usr/bin/nvim" >/etc/environment
+
+echo "Completed!"
